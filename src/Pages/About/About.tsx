26b6a51373992942911ecfd65ca541844632a4cb
Skailodon <jackiehtruong@outlook.com>
2024-10-04 21:29:58 -0700

    Tailwind installation

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
+import './about.css'
+import Post from '../../components/templates/Post/Post'
 
 export default function About() {
     //DO NOT CHANGE BODY AND CONTENT, MARGINS ARE DEFINED THERE
     return (
         <>
             <body>
-                <div className='content'>
-                    <div>About</div>
+                <div className='content aboutBox'>
+                    <ol>
+                        <li className='faqHeading'>What is this site about?</li>
+                        <p>This site is helps you organize your favourite songs into organized playlists</p>
+                        <li className='faqHeading'>What is this site about?</li>
+                        <p>This site is helps you organize your favourite songs into organized playlists</p>
+                        <li className='faqHeading'>What is this site about?</li>
+                        <p>This site is helps you organize your favourite songs into organized playlists</p>
+                    </ol>
+                    <Post />
                 </div>
             </body>
         </>
