1127b8520612ce00b19e644b8131a049e2a7bef0
Skailodon <jackiehtruong@outlook.com>
2024-10-06 23:27:49 -0700

    Card about page card component styled in Card.tsx

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Card from '../../components/templates/Card/Card'
+import Styles from './About.module.css'
 
 export default function About() {
     //DO NOT CHANGE BODY AND CONTENT, MARGINS ARE DEFINED THERE
@@ -6,7 +8,13 @@ export default function About() {
         <>
             <body>
                 <div className='content'>
-                    <div>About</div>
+                    <h1>About Turntable</h1>
+                    <div className={Styles.aboutLayout}>
+                        <p className={Styles.aboutDescription}>
+                            Turntable is a personalized music app designed to help you curate and explore your favorite songs in a unique way. With Turntable, you can save links to tracks that matter to you, preview songs directly within the app, and organize your music into custom playlists. For an added layer of creativity, you can even moodboard your songs, visually capturing the emotions or memories tied to each track. Whether you're building a playlist for every mood or reminiscing on a special tune, Turntable makes managing your music personal and fun.\
+                        </p>
+                        <Card />
+                    </div>
                 </div>
             </body>
         </>
---
+++ src/components/templates/Card/Card.tsx
@@ -0,0 +1,25 @@
+import styles from './Card.module.css'
+import Image from 'react'
+
+export default function Card() {
+    return (
+        <div className={styles.cardRelative}>
+            <div className={styles.cardContainer}>
+                <img className={styles.cardImage} src='https://placehold.co/600x400' />
+                <div className={styles.cardInformation}>
+                    <div className={styles.cardNameContainer}>
+                        <img className={styles.cardProfile} src='https://placehold.co/600x400'></img>
+                        <div className={styles.cardName}>
+                            John Doe
+                        </div>
+                    </div>
+                    <div className={styles.cardLine}></div>
+                    <p className={styles.cardText}>
+                        Lorem Ipsum Lorem Ipsum  Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum                         Lorem Ipsum Lorem Ipsum  Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
