972be98f404110894964449db4175f6060c4638b
Skailodon <jackiehtruong@outlook.com>
2024-10-08 22:03:53 -0700

    Added card to contact page

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -1,44 +1,33 @@
 import React from 'react'
 import Styles from "./Contact.module.css"
 import { SocialIcon } from 'react-social-icons'
+import ContactForm from '../../components/templates/ContactForm/ContactForm'
+import Card from '../../components/templates/Card/Card'
 
 export default function Contact() {
     //DO NOT CHANGE BODY AND CONTENT, MARGINS ARE DEFINED THERE
     return (
         <>
             <body>
-                <div className='content'>
-                    <h1>Contact Us!</h1>
-                    <form className={Styles.formContainer}>
-                        <p className={Styles.subhead}>CONTACT</p>
-                        <div>
-                            <label for="email">Email:</label>
-                            <input type='text' id='email' name='email' className={Styles.emailLine} />
-                        </div>
-                        <div>
-                            <label for="subject">Subject:</label>
-                            <input type='text' id='subject' name='subject' className={Styles.subjectLine} />
-                        </div>
-                        <div>
-                            <label for="message">Message:</label>
-                            <textarea type='text' id='message' name='message' className={Styles.messageLine} />
-                        </div>
-                        <button className={Styles.btnSend}>Send</button>
-                    </form>
-                    <div className='mt-5'>
-                        <h1 className=''>We're also here!</h1>
-                        <div className='mt-3'>
-                            <SocialIcon url="https://x.com" bgColor='none' />
-                            <SocialIcon url="https://tiktok.com" bgColor='none' />
-                            <SocialIcon url="https://twitch.tv" bgColor='none' />
-                            <SocialIcon url="https://youtube.com" bgColor='none' />
-                            <SocialIcon url="https://instagram.com" bgColor='none' />
-                            <SocialIcon url="https://spotify.com" bgColor='none' />
+                <div className='content flex flex-row'>
+                    <div>
+                        <h1>Contact Us!</h1>
+                        <ContactForm />
+                        <div className='mt-5'>
+                            <h1 className=''>We're also here!</h1>
+                            <div className='mt-3'>
+                                <SocialIcon url="https://x.com" bgColor='none' />
+                                <SocialIcon url="https://tiktok.com" bgColor='none' />
+                                <SocialIcon url="https://twitch.tv" bgColor='none' />
+                                <SocialIcon url="https://youtube.com" bgColor='none' />
+                                <SocialIcon url="https://instagram.com" bgColor='none' />
+                                <SocialIcon url="https://spotify.com" bgColor='none' />
+                            </div>
                         </div>
                     </div>
-
+                    <Card />
                 </div>
-            </body>
+            </body >
         </>
     )
 }
