72c6722e0db9d729e9b9a0a68d1e5f384ee49257
Ben Louis <142682578+Sucralosee@users.noreply.github.com>
2024-10-11 17:40:56 -0700

    added css to make home not scrollable

--- src/components/molecules/Focus.jsx
+++ src/components/molecules/Focus.jsx
@@ -1,4 +1,5 @@
 import './focus.css';
+import './song.css'
 
 export default function Focus({ selectedSong }) {
     return (
@@ -6,12 +7,19 @@ export default function Focus({ selectedSong }) {
             {/* <h2>Highlighted Song</h2> */}
             <div className='focus-song flex'>
                 <div className='focus-image flex'>
-                    <img src={selectedSong.image_link} alt={selectedSong.title} />
+                    <img 
+                        src={selectedSong.image_link} 
+                        alt={selectedSong.title} 
+                        className='focus-image-link'
+                    />
                 </div>
                 <div className='focus-info flex'>
                     <p className='focus-item focus-title'>{selectedSong.title}</p>
-                    <p className='focus-item focus-author'>{selectedSong.author}</p>
-                    <a className='focus-item focus-link' href={selectedSong.song_link} target="_blank">Listen</a>
+                    <div className='focus-item focus-wide flex'>
+                        <p className='focus-item song-author focus-author'>{selectedSong.author}</p>
+                        {/* <p className='focus-item focus-author focus-date'>{selectedSong.date}</p>*/}
+                        <a className='focus-item song-link focus-link' href={selectedSong.song_link} target="_blank">Listen</a>
+                    </div>
                     <p className='focus-item focus-desc'>{selectedSong.song_desc}</p>
                 </div>
             </div>
