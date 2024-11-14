b591df06ba7cafa7a5925fdbc801dad42ab79d1b
Skailodon <jackiehtruong@outlook.com>
2024-11-13 23:18:58 -0800

    inserted local storage control within home.tsx for songs

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./home.css"
 import Song from '../../components/molecules/song';
 import Focus from '../../components/molecules/Focus';
@@ -6,50 +6,44 @@ import { FaPlus, FaBars } from "react-icons/fa";
 import { CgPlayListAdd } from 'react-icons/cg';
 import { nanoid } from 'nanoid';
 import AddSong from '../../components/templates/AddSong/AddSong';
+import { json } from 'react-router-dom';
 
 export default function Home() {
-    const [songs, setSongs] = useState([
-        {
-            id: nanoid(),
-            title: 'Pure Blood',
-            image_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4CIO3epP3But06P9xsDPvknJy59JGCQ9OKQ&s",
-            author: "Dareharu",
-            song_link: "https://www.youtube.com/watch?v=en78bBw-Ekw",
-            song_desc: "달의하루(Dareharu) 『순혈주의자(Pure Blood)"
-        },
-        {
-            id: nanoid(),
-            title: 'jschlatt — Santa Claus Is Coming To Town (Official)',
-            image_link: "https://i.ytimg.com/vi/hayoY2Ov8PM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCccpnkxaz6_w9igkfcX01UFCAlSQ",
-            author: "JSchlatt",
-            song_link: "https://www.youtube.com/watch?v=hayoY2Ov8PM",
-            song_desc: "MERRY CHRISTMAS BABY!!!!!!"
-        },
-        {
-            id: nanoid(),
-            title: 'みゆな - ガムシャラ【Official Music Video】',
-            image_link: "https://i.ytimg.com/vi/TciQvQFTzHo/maxresdefault.jpg",
-            author: "Miyuna",
-            song_link: "https://www.youtube.com/watch?v=TciQvQFTzHo",
-            song_desc: "TVアニメ「ブラッククローバー」第５クールオープニングテーマガムシャラ 楽曲配信はこちら ⇒ https://MIYUNA.lnk.to/MIYUNA_GAMUSYARA"
-        },
-        {
-            id: nanoid(),
-            title: 'みゆな - ガムシャラ【Official Music Video】',
-            image_link: "https://i.ytimg.com/vi/TciQvQFTzHo/maxresdefault.jpg",
-            author: "Miyuna",
-            song_link: "https://www.youtube.com/watch?v=TciQvQFTzHo",
-            song_desc: "TVアニメ「ブラッククローバー」第５クールオープニングテーマガムシャラ 楽曲配信はこちら ⇒ https://MIYUNA.lnk.to/MIYUNA_GAMUSYARA"
-        },
-        {
-            id: nanoid(),
-            title: 'みゆな - ガムシャラ【Official Music Video】',
-            image_link: "https://i.ytimg.com/vi/TciQvQFTzHo/maxresdefault.jpg",
-            author: "Miyuna",
-            song_link: "https://www.youtube.com/watch?v=TciQvQFTzHo",
-            song_desc: "TVアニメ「ブラッククローバー」第５クールオープニングテーマガムシャラ 楽曲配信はこちら ⇒ https://MIYUNA.lnk.to/MIYUNA_GAMUSYARA"
-        },
-    ]);
+    const [songs, setSongs] = useState(() => {
+        const savedSongs = localStorage.getItem("song");
+        return savedSongs ? JSON.parse(savedSongs) : [
+            {
+                id: nanoid(),
+                title: 'Pure Blood',
+                image_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4CIO3epP3But06P9xsDPvknJy59JGCQ9OKQ&s",
+                author: "Dareharu",
+                song_link: "https://www.youtube.com/watch?v=en78bBw-Ekw",
+                song_desc: "달의하루(Dareharu) 『순혈주의자(Pure Blood)"
+            },
+            {
+                id: nanoid(),
+                title: 'jschlatt — Santa Claus Is Coming To Town (Official)',
+                image_link: "https://i.ytimg.com/vi/hayoY2Ov8PM/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCccpnkxaz6_w9igkfcX01UFCAlSQ",
+                author: "JSchlatt",
+                song_link: "https://www.youtube.com/watch?v=hayoY2Ov8PM",
+                song_desc: "MERRY CHRISTMAS BABY!!!!!!"
+            },
+            {
+                id: nanoid(),
+                title: 'みゆな - ガムシャラ【Official Music Video】',
+                image_link: "https://i.ytimg.com/vi/TciQvQFTzHo/maxresdefault.jpg",
+                author: "Miyuna",
+                song_link: "https://www.youtube.com/watch?v=TciQvQFTzHo",
+                song_desc: "TVアニメ「ブラッククローバー」第５クールオープニングテーマガムシャラ 楽曲配信はこちら ⇒ https://MIYUNA.lnk.to/MIYUNA_GAMUSYARA"
+            },
+        ];
+    });
+
+    const [localSongs, setLocalSongs] = useState(() => {
+        const savedSongs = localStorage.getItem("song");
+        const initialValue = savedSongs ? JSON.parse(savedSongs) : [];
+        return initialValue;
+    });
 
     const [selectedSong, setSelectedSong] = useState(songs[0]);
     const [newSong, setNewSong] = useState({
@@ -93,6 +87,11 @@ export default function Home() {
     function toggleAddSong() {
         setAddSongOverlay(!addSongOverlay)
     }
+
+    useEffect(() => {
+        return () => localStorage.setItem("song", JSON.stringify(songs))
+    }, [songs])
+
 
     return (
         <div className='content'>
