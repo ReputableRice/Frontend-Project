841212100fd299e50f4336dc286ae21e04d0009a
Skailodon <jackiehtruong@outlook.com>
2024-12-01 17:20:08 -0800

    changed landing route from App to Home

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Preloader from "./components/templates/Preloader/Preloader.tsx";
 import "./index.css";
+import Home from "./Pages/Home/Home.tsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Preloader />} />
-        <Route path="/*" element={<App />} />
+        <Route path="/*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
