db5da10b47bca0e87a9f0cc8d2f2b04b70382091
Skailodon <jackiehtruong@outlook.com>
2024-12-01 18:05:19 -0800

    disc fix

--- src/components/molecules/disc/Disc.tsx
+++ src/components/molecules/disc/Disc.tsx
@@ -1,21 +1,24 @@
 import React, { useEffect } from "react"
 import { useState } from "react"
 import styles from "./disc.module.css"
+import playing from "../../../../public/playing.png"
+import paused from "../../../../public/playing.png"
+
 
 export default function Disc({ focus }) {
     const [isPlaying, setIsPlaying] = useState("")
 
     useEffect(() => {
         if (focus) {
-            setIsPlaying("../../public/playing.png")
+            setIsPlaying(playing)
         } else {
-            setIsPlaying("../../public/paused.png")
+            setIsPlaying(paused)
         }
-    })
+    }, [focus])
 
     return (
         <>
-            <img src={isPlaying} className={`fixed z-50 w-11 absolute ${styles.discPosition}`}/>
+            <img src={isPlaying} className={`fixed z-50 w-11 absolute ${styles.discPosition}`} alt="a spinning music disc" />
         </>
     )
 }
